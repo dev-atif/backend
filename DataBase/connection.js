e13cc9944043d961d4471d15f61d3851e5508f76
dev-atif <atifali5410@gmail.com>
2023-10-17 05:23:30 -0700

    sdfsdf

--- DataBase/connection.js
+++ DataBase/connection.js
@@ -3,19 +3,9 @@ mongoose.connect("mongodb+srv://root:root5410@cluster0.buldado.mongodb.net/Local
  */
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://root:root5410@cluster0.buldado.mongodb.net/LocalTools", {
+mongoose.connect("mongodb+srv://root:root5410@cluster0.buldado.mongodb.net/LocalTools?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-
-// Event handler for a successful connection
-db.once('open', () => {
-  console.log('MongoDB connection established successfully');
-});
-
-// Event handler for a connection error
-db.on('error', (error) => {
-  console.error('MongoDB connection error:', error);
-});
+}).then(()=>{console.log('MongoDB connected')}).catch((err)=>{
+  console.log('Mongodb Not connected--->',err.message)
+})
